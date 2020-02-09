b96d85d40dd3981401aa179fef04f7aef6ff382f
KaiYue <eric.kaiyue@gmail.com>
2020-02-09 15:31:22 +0800

    update test

--- src/test.py
+++ src/test.py
@@ -52,7 +52,7 @@ def HLo_test(model, output_dir, device="cuda", mode=0):
                     img = cv2.resize(img, tuple(config.input_size))
                     
                     Tensor_img = pre_process(img/255).to(device)
-                    result, _ = model(Tensor_img)
+                    result = model(Tensor_img)
 
                     result = np.squeeze(result.cpu().detach().numpy())
                     result = cv2.resize(result, tuple(config.input_size))
@@ -95,8 +95,8 @@ def HLo_test(model, output_dir, device="cuda", mode=0):
                         Tensor_img = pre_process(img).to(device)
                         Tensor_hm = pre_process(hm).to(device)
 
-                        result, interm = model(Tensor_img)
-                        loss = L(result, Tensor_hm, interm)
+                        result = model(Tensor_img)
+                        loss = L(result, Tensor_hm)
                         
                         result = np.squeeze(result.cpu().detach().numpy())
                         result = cv2.resize(result, tuple(config.input_size))
@@ -153,8 +153,8 @@ def PRe_test(model, output_dir, device="cuda"):
                     Tensor_hm = Tensor_hm[None, ...]
                     Tensor_pos = torch.from_numpy(a_set['pos_arr']).view(1,21,3,1).to(torch.float32).to(device)
 
-                    result, interm = model(Tensor_img)
-                    loss, _ = L(result[0], interm, result[1], Tensor_hm, Tensor_pos)
+                    result = model(Tensor_img)
+                    loss, _ = L(result[0], result[1], Tensor_hm, Tensor_pos)
                     pos_arr = pos_from_heatmap(result[0].squeeze())
 
                     result = np.squeeze(result[0].cpu().detach().numpy())
@@ -203,14 +203,14 @@ def PRe_test(model, output_dir, device="cuda"):
                     break
 
 
-def Synth_test(Hal, Jor, input_dir, output_dir, device="cuda"):
+def Synth_test(HLo, PRe, input_dir, output_dir, device="cuda"):
     """
-    Test the joint model on SynthHand dataset. First the Hal model localize the hand image, then 
-    the Jor model regresses the joint position.
+    Test the joint model on SynthHand dataset. First the HLo model localize the hand image, then 
+    the PRe model regresses the joint position.
     --------------------------------------------------------------------------------
     Args,
-        Hal,       the hand localization model.
-        Jor,       the joint regression model.
+        HLo,       the hand localization model.
+        PRe,       the position regression model.
         input_dir, the input directory of the test set (already transformed.) The grountruth is known.  
     """
 
@@ -241,7 +241,7 @@ def Synth_test(Hal, Jor, input_dir, output_dir, device="cuda"):
                     depth = a_set["depth"]
                     Tensor_img = pre_process(img).to(device)
 
-                    result, interm = Hal(Tensor_img)
+                    result = HLo(Tensor_img)
                     center = center_from_heatmap(result.squeeze())
 
                     # Back projection with th predicted center
@@ -283,7 +283,7 @@ def Synth_test(Hal, Jor, input_dir, output_dir, device="cuda"):
                     # Regress the joint position
                     Tensor_img = pre_process(cropped_hand).to(device)
 
-                    result, interm = Jor(Tensor_img)
+                    result, interm = PRe(Tensor_img)
                     pred_pos = pos_from_heatmap(result[0].squeeze())
 
                     pos_3d = result[1].cpu().detach().squeeze().numpy()
@@ -319,18 +319,114 @@ def Synth_test(Hal, Jor, input_dir, output_dir, device="cuda"):
                     break
 
 
-def Ego_test(Hal, Jor, input_dir, output_dir, device="cuda"):
+def Ego_test(HLo, PRe, input_dir, output_dir, device="cuda"):
     """
-    Test the joint model on EgoDexter dataset. First the Hal model localize the hand image, then 
-    the Jor model regresses the joint position.
+    Test the joint model on EgoDexter dataset. First the HLo model localize the hand image, then 
+    the PRe model regresses the joint position.
     --------------------------------------------------------------------------------
     Args,
-        Hal,       the hand localization model.
-        Jor,       the joint regression model.
+        HLo,       the hand localization model.
+        PRe,       the joint regression model.
         input_dir, the input directory of the test set (already transformed.) The grountruth is unknown.  
     """
     alpha = 0.6
     config = loadConfig()
+
+    for root, dirs, files in os.walk(input_dir):
+        
+        new_dir = os.path.join(root.replace(input_dir, output_dir))
+        if not os.path.exists(new_dir):
+            os.mkdir(new_dir)
+
+        if (files != [] and "color_on_depth" in root):
+            sampled_in_folder = 0
+            indices = np.arange(len(files))
+            # random.shuffle(indices)
+
+            for i in range(config.test_samples):
+                f = files[indices[i]]
+                img = cv2.imread(os.path.join(root, f))[:,:,::-1]
+                img = cv2.resize(img, tuple(config.input_size))
+                
+                Tensor_img = pre_process(img/255).to(device)
+                result = HLo(Tensor_img)
+                
+                center = center_from_heatmap(result.squeeze())
+
+                # Back projection with th predicted center
+                d = depth[center[1], center[0]]
+                x_3d = (center[0] - x_0) * d / f_x
+                y_3d = (center[1] - y_0) * d / f_y
+
+                hm = result.squeeze().detach().cpu().numpy()
+                hm = cv2.resize(hm, tuple(config.input_size))
+                img = (255*img).astype("uint8")
+                heatmap = Heatmap(hm)
+                composite = alpha*img + (1-alpha)*heatmap[...,::-1]
+                
+                # Plot the result 
+                fig, axs = plt.subplots(nrows=plot_rows, ncols=plot_cols, figsize=(15, 10))
+                axs[0,0].set_axis_off()
+                axs[0,0].set_title("Original image")
+                axs[0,0].imshow(img)
+                
+                axs[1,0].set_axis_off()
+                axs[1,0].set_title("Predicted {}".format(center))
+                axs[1,0].imshow(composite.astype("uint8"))
+                
+                # Load the pos array
+                f_ = f.replace(".dat", "_joint_pos.txt")
+                pos_arr = np.loadtxt(os.path.join(root, f_))
+
+                # Plot the cropped hand
+                ROI = ROI_Hand(img, depth, center)
+                cropped_hand = img[ROI[0]:ROI[1], ROI[2]:ROI[3]]
+                axs[0,1].set_axis_off()
+                axs[0,1].set_title("Cropped hm")
+                axs[0,1].imshow(cropped_hand)
+                
+                # modify the pos array.
+                pos_arr[21:, 0] -= ROI[2]
+                pos_arr[21:, 1] -= ROI[0]
+
+                # Regress the joint position
+                Tensor_img = pre_process(cropped_hand).to(device)
+
+                result, interm = PRe(Tensor_img)
+                pred_pos = pos_from_heatmap(result[0].squeeze())
+
+                pos_3d = result[1].cpu().detach().squeeze().numpy()
+                pos_3d += np.array(x_0, y_0, d)
+
+                # plot the original 2-D links
+                axs[1, 1].set_axis_off()
+                axs[1, 1].set_title("Links GT")
+                plot_joint(cropped_hand, pos_arr[21:, :2], axs[0, 2])
+                
+                # Plot the 2-D links results
+                axs[0, 2].set_axis_off()
+                axs[0, 2].set_title("2D Links ")  
+                plot_joint(cropped_hand, pred_pos, axs[0, 2])
+                
+                # Plot the 3-D links results
+                axs[1, 2].set_axis_off()
+                axs[1, 2].set_title("3D Links")
+                plot_joint(cropped_hand, pred_pos, axs[1, 2])
+
+                error = np.sqrt(np.sum((pos_arr[21:, :2]-pred_pos)**2))
+                
+                fig.savefig(os.path.join(new_dir, f[:8] + "_{:.2f}.jpg".format(error)))
+                plt.close(fig)
+
+                sampled_in_folder += 1
+
+                if (sampled_in_folder > config.samples_per_folder):
+                    break
+        
+            already_sampled += sampled_in_folder
+            if (sampled_in_folder > config.test_samples):
+                break
+
 
 f_x = 475.62
 f_y = 475.62
